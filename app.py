a132ddab5c23d2bc8d918003fd70f112e39b5361
Achu <achyuth10@gmail.com>
2018-08-19 16:41:42 +0530

    delete proc file

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def formhandler():
 	old = request.forms.get('old')
 	new = request.forms.get('new')
 	new_para = para.replace(old,new)
-	return {"para" : new_para}				
+	return{"para" : new_para}				
 
 
 run(host='0.0.0.0', port=int(os.environ.get("PORT",5000)))					#opens it to the network
